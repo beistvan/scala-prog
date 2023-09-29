640a2147e70ef420d029f09d9d02a96f39cb725f
Beregszászi István <beistvan@gmail.com>
2023-09-29 21:41:33 +0200

    Variables And Values

---
+++ variables-and-values.scala
@@ -0,0 +1,16 @@
+/* Variables And Values In Scala */
+object HelloWorld {
+    def main(args: Array[String]) {
+        val a: Int = 3
+        val b = true
+        var c = 'g'
+        val d = 2.8
+        val s = "Hi!"
+        
+        println("val a = " + a + " is " + a.getClass)
+        println("var b = " + b + " is " + b.getClass)
+        println("var c = " + c + " is " + c.getClass)
+        println("var d = " + d + " is " + d.getClass)
+        println("var s = " + s + " is " + s.getClass)
+    }
+}
