ffa7b3ed7810eed626df3159cf39c58bf0b91bf5
Beregszászi István <beistvan@gmail.com>
2023-09-30 12:51:44 +0200

    Some String Methods

---
+++ SomeStringMethods.scala
@@ -0,0 +1,10 @@
+/* Some String Methods */
+object SomeStringMethods {
+    def main(args: Array[String]) {
+        val hello: String = "Hello"
+        
+        println("val hello  = " + hello)
+        println("hello - to upper case: " + hello.toUpperCase())
+        println("hello - length: " + hello.length())
+    }
+}
