ac26ec1229088854ccc552e2425ddbfeed690c25
Beregszászi István <beistvan@gmail.com>
2023-10-01 20:44:01 +0200

    Some Integers

---
+++ SomInts.scala
@@ -0,0 +1,12 @@
+/* Some Ints */
+object SomInts {
+    def main(args: Array[String]) {
+        val a = 100
+        val b = 200
+        var c = a * b
+        
+        println("val a  = " + a)
+        println("val b  = " + b)
+        println("val c  = " + c)
+    }
+}
